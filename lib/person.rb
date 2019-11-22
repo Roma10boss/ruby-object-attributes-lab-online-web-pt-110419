e21f8921beef3311cb18a5b3c520e723aa66b09d
Romario Gustave <romagustave10@gmail.com>
2019-11-22 14:27:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,17 @@
 class Person
  
-  def initialize(first_name, last_name)
-    @name = name
-  end
- 
-  def name=(name)
-    name = name.split
-    @name = name
-  
+  def name(person_name)
+    @name = person_name
   end
  
   def name
-    " #{@name}".strip
-  end
- 
-end
-def initialize (job)
-  @job=job
+    @name
+  end 
+  def job(person_job)
+  @job=person_jobjob
 end 
-def job= (job)
-  @job=job.split
-  @job=job
+def job
+  @job
 end
 defjob
   "#{@job}".strip
